92f2b9de1ff42af53e864b9c00e250537fc533fd
iki1uc <entertrainer@web.de>
2026-07-06 02:55:04 +0200

    Update axi.routes.js

--- routes/axi.routes.js
+++ routes/axi.routes.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const axi = require('../core/axi_link');
+
+router.get('/modules', (req, res) => {
+    res.json(axi.modules());
+});
+
+module.exports = router;
 
