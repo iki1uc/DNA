49525537a67b887182ffec0185f7ceb07e86629a
iki1uc <entertrainer@web.de>
2026-07-06 02:53:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use('/px', require('./routes/px.routes'));
+app.use('/axi', require('./routes/axi.routes'));
+app.use('/ghost', require('./routes/ghost.routes'));
+app.use('/pipe', require('./routes/pipe.routes'));
+app.use('/tp', require('./routes/tp.routes'));
+
+app.listen(3000, () => {
+    console.log("DNA KOOP Router aktiv");
+});
 
