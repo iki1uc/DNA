85e918a1d4d9e63d6a8f3241b46ee74b84de4d49
iki1uc <entertrainer@web.de>
2026-07-06 02:55:57 +0200

    Update pipe.routes.js

--- routes/pipe.routes.js
+++ routes/pipe.routes.js
@@ -1,1 +1,9 @@
 
+const express = require('express');
+const router = express.Router();
+
+router.get('/flow', (req, res) => {
+    res.json({ pipe: "Pipeline aktiv" });
+});
+
+module.exports = router;
