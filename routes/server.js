38dacfa996e7666960df99f1a68d6f6f23267baa
iki1uc <entertrainer@web.de>
2026-07-06 02:53:18 +0200

    Delete routes/server.js

--- routes/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use('/px', require('./routes/px.routes'));
-app.use('/ghost', require('./routes/ghost.routes'));
-app.use('/axi', require('./routes/axi.routes'));
-
-app.listen(3000);
-
