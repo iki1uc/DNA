2bf3278984104d2c60e5a3c4f2a4de568106ce93
iki1uc <entertrainer@web.de>
2026-07-06 02:55:29 +0200

    Add scan route to ghost.routes.js

--- routes/ghost.routes.js
+++ routes/ghost.routes.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/scan', (req, res) => {
+    res.json({ ghost: "Scan gestartet" });
+});
+
+module.exports = router;
 
